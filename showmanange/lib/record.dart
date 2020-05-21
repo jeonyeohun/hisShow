297bd463a84782e309208c99abef56e68100ecaf
jeonyeohun <21500630@handong.edu>
2020-05-21 10:59:46 +0900

    set documents of database

--- showmanange/lib/record.dart
+++ showmanange/lib/record.dart
@@ -3,19 +3,24 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Record {
   final String title;
-  final Timestamp start_date;
-  final Timestamp end_date;
+  final DateTime date;
   final String description;
-  final String create_uid;
-  final List<dynamic> vote_list;
-  final String image_uid;
+  final String uid;
+  final List<dynamic> voteList;
+  final String imageURL;
   final List<List<String>> sits;
 
   final DocumentReference reference;
 
   Record.fromMap(Map<String, dynamic> map, {this.reference})
       : assert(map['title'] != null),
-   
+      title = map['title'],
+  date = map['date'],
+  description = map['description'],
+  uid = map['uid'],
+  voteList = map['voteList'],
+  imageURL = map['imageURL'],
+  sits = map['sits'];
 
 
   Record.fromSnapshot(DocumentSnapshot snapshot)
